3f7a921eae5e6cda42642254082c2ebe081fb885
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-03 17:30:50 +0000

    Day 2

---
+++ 02/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Line = { Min: int; Max: int; Char: char; Pass: string }
+
+[<EntryPoint>]
+let main argv =
+
+    let parseLine line =
+        let pattern = "(\d+)-(\d+) (.): (.+)"
+        let m = Regex.Match(line, pattern)
+        let [min;max;char;pass] = List.tail [for g in m.Groups -> g.Value]
+        { Min = Int32.Parse min; Max = Int32.Parse max; Char = char.[0]; Pass = pass}
+
+    let isValid line =
+        let count =
+            line.Pass
+            |> Seq.filter (fun c -> c = line.Char)
+            |> Seq.length
+        line.Min <= count && count <= line.Max
+
+    File.ReadAllLines("input")
+    |> Array.map parseLine
+    |> Array.filter isValid
+    |> Array.length
+    |> printf "%i"
+
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2020
 
 https://adventofcode.com/2020
+
+To run the programs, do `dotnet run` in one of the numbered folders.
