ea2b608b69651269d656d1d06e00b0f9b58e9e5a
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-04 07:09:54 +0000

    Day 4 - Part 1

---
+++ 04-1/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+
+    let input = File.ReadAllText("input")
+    
+    let passportFromString (str:string) =
+        str.Split([| "\n"; " " |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun field -> field.Split(':').[0])
+        |> Set.ofArray
+
+    let isValid passport =
+        let requiredFields = set [ "byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid" ]
+        Set.isSuperset passport requiredFields
+
+    input.Split([| "\n\n" |], StringSplitOptions.None)
+    |> Array.map passportFromString
+    |> Array.filter isValid
+    |> Array.length
+    |> printf "%i"
+
+    0 // return an integer exit code
