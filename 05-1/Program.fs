7d794d7ee6df214081fb7552103a1ea3bb73b89c
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-05 15:52:15 +0000

    Day 5 - Part 1

---
+++ 05-1/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+type Seat = { Row: int; Column: int }
+
+[<EntryPoint>]
+let main argv =
+
+    let input = File.ReadAllLines("input")
+    
+    let parseSeat (s:string) =
+        let binary = s.Replace("F", "0").Replace("B", "1").Replace("L", "0").Replace("R", "1")
+        { Row = Convert.ToInt32(binary.[..6], 2); 
+          Column = Convert.ToInt32(binary.[7..], 2) }
+    
+    let seatId (seat:Seat) =
+        seat.Row * 8 + seat.Column
+
+    input
+    |> Array.map (parseSeat >> seatId)
+    |> Array.max
+    |> printf "%i"
+
+    0 // return an integer exit code
