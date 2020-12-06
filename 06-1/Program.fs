cc6ef5c115bb1206b20122551f4e27ea80714fa2
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-06 10:10:13 +0000

    Day 6 - Part 1

---
+++ 06-1/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+
+    let input = File.ReadAllText("input")
+    let groups = input.Split("\n\n")
+
+    let count (group:string) =
+        set [for c in group.Replace("\n", "") -> c]
+        |> Set.count
+    
+    groups
+    |> Array.sumBy count 
+    |> printfn "%i"
+
+    0 // return an integer exit code
