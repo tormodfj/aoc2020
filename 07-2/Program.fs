ffd82613f5105a5f5ea314d915eb5b1c11c0b181
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-07 13:59:32 +0000

    Day 7 - Part 2

---
+++ 07-2/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+[<EntryPoint>]
+let main argv =
+
+    let input = File.ReadAllLines("input")
+
+    let parse line =
+        let parseContent content =
+            match content with
+            | Regex "(\d+) (.*) bag" [ count; color ] -> Some(Int32.Parse count, color)
+            | _ -> None
+        match line with
+        | Regex "(.*) bags contain no other bags" [ color ] -> Some(color, [||])
+        | Regex "(.*) bags contain (.*)\." [color; content] -> Some(color, content.Split(",") |> Array.choose parseContent)
+        | _ -> None
+
+    let bags =
+        input
+        |> Array.choose parse
+        |> Map.ofArray 
+
+    let rec countBags bagColor =
+        let innerCount (count, color) = count + count * countBags color
+        bags.[bagColor] 
+        |> Array.sumBy innerCount
+
+    countBags "shiny gold"
+    |> printfn "%i"
+    
+    0 // return an integer exit code
