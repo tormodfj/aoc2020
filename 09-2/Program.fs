c98dcdf4389ace756e196b7704750c1c323ffd0f
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-09 15:59:17 +0000

    Day 9 - Part 2

---
+++ 09-2/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+
+    let input = 
+        File.ReadAllLines("input")
+        |> Array.map Int64.Parse
+        |> List.ofArray
+
+    let targetSum = 1212510616L
+
+    let takeUntilTargetSum list =
+        let rec takeUntil acc sum list =
+            match list with
+            | head::tail when head + sum < targetSum 
+                -> takeUntil (head::acc) (head + sum) tail
+            | head::_ when head + sum = targetSum -> Some acc
+            | _ -> None 
+        takeUntil [] 0L list
+    
+    let rec solve list =
+        match list, takeUntilTargetSum list with
+        | _, Some(result) -> result
+        | _::tail,_ -> solve tail
+        | [],_ -> failwith "No solution found"
+
+    let solution = solve input
+    let min, max = List.min solution, List.max solution
+
+    min + max
+    |> printfn "%i"
+
+    0 // return an integer exit code
