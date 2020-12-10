97f61cd9284fa47ec645b341af65bfbdcf00e8c2
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-10 08:17:56 +0000

    Day 10 - Part 1

---
+++ 10-1/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+
+    let input = 
+        File.ReadAllLines("input")
+        |> Array.map int
+        |> List.ofArray
+
+    let outlet, device = 0, 3 + List.max input
+
+    let result =
+        outlet::device::input
+        |> List.sort
+        |> List.pairwise
+        |> List.map (fun (x,y) -> y-x)
+        |> List.groupBy id
+        |> List.map (fun (i,lst) -> (i, lst.Length))
+        |> Map.ofList
+
+    result.[1] * result.[3]
+    |> printfn "%i"
+
+    0 // return an integer exit code
