c35f459d229ae1889f25df2d208e7f1908bdad9e
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2020-12-10 19:33:02 +0000

    Day 10 - Part 2

---
+++ 10-2/Program.fs
@@ -0,0 +1,41 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+
+    let input = 
+        File.ReadAllLines("input")
+        |> Array.map int
+        |> List.ofArray
+
+    let outlet, device, adapters = 0, 3 + List.max input, input
+    let nodes = List.sort (outlet::device::adapters)
+
+    let getCandidates i =
+        let isPredecessor x = x <= i
+        let isCandidate x = x <= i + 3
+
+        nodes
+        |> List.skipWhile isPredecessor
+        |> List.takeWhile isCandidate
+    
+    let graph = 
+        nodes 
+        |> List.map (fun i -> (i, getCandidates i))
+
+    let rec solve (pathCountMap:Map<int,int64>) graph =
+        match graph with
+        | [] -> pathCountMap.[0]    
+        | (node, candidates)::tail -> 
+            let pathCount = 
+                candidates 
+                |> List.sumBy (fun n -> pathCountMap.[n])
+            solve (pathCountMap |> Map.add node pathCount) tail
+
+    graph 
+    |> List.rev 
+    |> List.tail
+    |> solve (Map[device, 1L]) 
+    |> printfn "%i"
+
+    0 // return an integer exit code
